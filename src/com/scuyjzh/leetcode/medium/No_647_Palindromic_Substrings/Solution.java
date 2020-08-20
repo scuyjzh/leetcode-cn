e3e41a8eab4c0f108c4fdbb3113f2a894ff86608
scuyjzh <scuyjzh@163.com>
2020-08-21 00:50:12 +0800

    update

--- src/com/scuyjzh/leetcode/medium/No_647_Palindromic_Substrings/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_647_Palindromic_Substrings/Solution.java
@@ -6,11 +6,34 @@ package com.scuyjzh.leetcode.medium.No_647_Palindromic_Substrings;
  */
 class Solution {
     /**
-     * 方法一：中心拓展
-     * 时间复杂度：O(n^2)
-     * 空间复杂度：O(1)
+     * 方法一：动态规划
+     * 时间复杂度：O(N^2)
+     * 空间复杂度：O(N^2)
      */
     public int countSubstrings1(String s) {
+        int len = s.length();
+        // dp[i][j] 表示 s[i..j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+        int ans = 0;
+        for (int j = 0; j < len; j++) {
+            for (int i = 0; i <= j; i++) {
+                // 边界条件：j - 1 - (i + 1) + 1 < 2
+                if (charArray[i] == charArray[j] && (j - i < 3 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 方法二：中心扩散
+     * 时间复杂度：O(N^2)
+     * 空间复杂度：O(1)
+     */
+    public int countSubstrings2(String s) {
         int n = s.length(), ans = 0;
         // 长度为 n 的字符串会生成 2n-1 组回文中心 [l_i, r_i]
         for (int i = 0; i < 2 * n - 1; ++i) {
@@ -28,5 +51,8 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.countSubstrings1("madam"));
+        System.out.println(solution.countSubstrings2("madam"));
+        System.out.println(solution.countSubstrings1("reviver"));
+        System.out.println(solution.countSubstrings2("reviver"));
     }
 }
