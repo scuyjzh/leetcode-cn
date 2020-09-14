72cb278710d205d5643f31ab8236e9cecc84f844
scuyjzh <scuyjzh@163.com>
2020-09-14 17:25:17 +0800

    update

--- src/com/scuyjzh/leetcode/medium/No_139_Word_Break/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_139_Word_Break/Solution.java
@@ -3,6 +3,9 @@ package com.scuyjzh.leetcode.medium.No_139_Word_Break;
 import java.util.*;
 
 /**
+ * 139. 单词拆分
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ *
  * @author scuyjzh
  * @date 2020/6/25 0:19
  */
@@ -19,7 +22,7 @@ class Solution {
           翻译一下：前八位能否拆分取决于前五位能否拆分，加上五到八位是否属于字典
           （注意：i的顺序是从j-1 -> 0哦~
          */
-        Set<String> wordDictSet = new HashSet(wordDict);
+        Set<String> wordDictSet = new HashSet<>(wordDict);
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
         for (int i = 1; i <= s.length(); ++i) {
@@ -31,5 +34,11 @@ class Solution {
             }
         }
         return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<String> wordDict = Arrays.asList("apple", "pen");
+        System.out.println(solution.wordBreak("applepenapple", wordDict));
     }
 }
