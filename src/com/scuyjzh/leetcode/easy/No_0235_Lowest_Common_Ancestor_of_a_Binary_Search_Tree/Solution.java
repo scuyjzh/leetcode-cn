eac7a8d7b85f7338db993fa2a8ac58b5bf8d4ed9
scuyjzh <scuyjzh@163.com>
2020-09-27 10:58:56 +0800

    update

--- src/com/scuyjzh/leetcode/easy/No_0235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.java
+++ src/com/scuyjzh/leetcode/easy/No_0235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.java
@@ -2,67 +2,102 @@ package com.scuyjzh.leetcode.easy.No_0235_Lowest_Common_Ancestor_of_a_Binary_Sea
 
 import com.scuyjzh.leetcode.structure.TreeNode;
 
+import java.util.*;
+
+/**
+ * 235. 二叉搜索树的最近公共祖先
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * @author scuyjzh
+ * @date 2020/9/27 9:46
+ */
 class Solution {
     /**
-     * Approach #1 (Iteration)
+     * 方法一：两次遍历
      */
-    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p,
-                                          TreeNode q) {
-        // Value of p
-        int pVal = p.val;
-
-        // Value of q;
-        int qVal = q.val;
-
-        // Start from the root node of the tree
-        TreeNode node = root;
-
-        // Traverse the tree
-        while (root != null) {
-            // Value of ancestor/parent node.
-            int parentVal = node.val;
-
-            if (pVal < parentVal && qVal < parentVal) {
-                // 如果根节点的值比p.val和q.val都大，则它们的最低公共祖先节点在根节点的左子树中
-                node = node.left;
-            } else if (pVal > parentVal && qVal > parentVal) {
-                // 如果根节点的值比p.val和q.val都小，则它们的最低公共祖先节点在根节点的右子树中
-                node = node.right;
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        // 得到从根节点到被寻找节点 p 的路径
+        List<TreeNode> path_p = getPath(root, p);
+        // 得到从根节点到被寻找节点 q 的路径
+        List<TreeNode> path_q = getPath(root, q);
+        TreeNode ancestor = null;
+        // p 和 q 的最近公共祖先就是从根节点到它们路径上的「分岔点」，也就是最后一个相同的节点
+        for (int i = 0; i < path_p.size() && i < path_q.size(); ++i) {
+            // 找出最大的编号 i 满足 path_p[i] == path_q[i]
+            if (path_p.get(i) == path_q.get(i)) {
+                ancestor = path_p.get(i);
             } else {
-                // 如果根节点的值处于p.val和q.val之间，则根节点就是它们的最低公共祖先节点
-                return node;
+                break;
             }
         }
-        return null;
+        return ancestor;
+    }
+
+    private List<TreeNode> getPath(TreeNode root, TreeNode target) {
+        List<TreeNode> path = new LinkedList<>();
+        // 从根节点开始遍历
+        TreeNode node = root;
+        // 如果当前节点就是 p，那么成功地找到了节点
+        while (node.val != target.val) {
+            path.add(node);
+            if (target.val < node.val) {
+                // 如果当前节点的值大于 p 的值，说明 p 应该在当前节点的左子树，因此将当前节点移动到它的左子节点
+                node = node.left;
+            } else {
+                // 如果当前节点的值小于 p 的值，说明 p 应该在当前节点的右子树，因此将当前节点移动到它的右子节点
+                node = node.right;
+            }
+        }
+        path.add(node);
+        return path;
     }
 
     /**
-     * Approach #2 (Recursion)
+     * 方法二：一次遍历
      */
-    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p,
-                                          TreeNode q) {
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        // 从根节点开始遍历
+        TreeNode ancestor = root;
+        while (true) {
+            if (p.val < ancestor.val && q.val < ancestor.val) {
+                // 如果当前节点的值大于 p 和 q 的值，说明 p 和 q 应该在当前节点的左子树，因此将当前节点移动到它的左子节点
+                ancestor = ancestor.left;
+            } else if (p.val > ancestor.val && q.val > ancestor.val) {
+                // 如果当前节点的值小于 p 和 q 的值，说明 p 和 q 应该在当前节点的右子树，因此将当前节点移动到它的右子节点
+                ancestor = ancestor.right;
+            } else {
+                // 如果当前节点的值不满足上述两条要求，那么说明当前节点就是「分岔点」
+                // 此时，p 和 q 要么在当前节点的不同的子树中，要么其中一个就是当前节点
+                break;
+            }
+        }
+        return ancestor;
+    }
+
+    /**
+     * 方法三：递归法
+     */
+    public TreeNode lowestCommonAncestor3(TreeNode root, TreeNode p, TreeNode q) {
         return helper(root, p, q);
     }
 
     private TreeNode helper(TreeNode root, TreeNode p, TreeNode q) {
-        // Value of current node or parent node.
-        int parentVal = root.val;
-
-        // Value of p
-        int pVal = p.val;
-
-        // Value of q;
-        int qVal = q.val;
-
-        if (pVal > parentVal && qVal > parentVal) {
-            // If both p and q are greater than parent
+        if (root.val < p.val && root.val < q.val) {
             return helper(root.right, p, q);
-        } else if (pVal < parentVal && qVal < parentVal) {
-            // If both p and q are lesser than parent
-            return helper(root.left, p, q);
-        } else {
-            // We have found the split point, i.e. the LCA node.
-            return root;
         }
+        if (root.val > p.val && root.val > q.val) {
+            return helper(root.left, p, q);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = TreeNode.initBinaryTree("[6,2,8,0,4,7,9,null,null,3,5]");
+        TreeNode p = new TreeNode(2);
+        TreeNode q = new TreeNode(8);
+        System.out.println(solution.lowestCommonAncestor1(root, p, q).val);
+        System.out.println(solution.lowestCommonAncestor2(root, p, q).val);
+        System.out.println(solution.lowestCommonAncestor3(root, p, q).val);
     }
 }
