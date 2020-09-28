e3a6186741f4b552f1bf23d005d2fa4bae23d68f
scuyjzh <scuyjzh@163.com>
2020-09-28 12:17:33 +0800

    update

---
+++ src/com/scuyjzh/leetcode/medium/No_0116_Populating_Next_Right_Pointers_in_Each_Node/Solution.java
@@ -0,0 +1,132 @@
+package com.scuyjzh.leetcode.medium.No_0116_Populating_Next_Right_Pointers_in_Each_Node;
+
+import java.util.*;
+
+/**
+ * 116. 填充每个节点的下一个右侧节点指针
+ * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ *
+ * @author scuyjzh
+ * @date 2020/9/28 12:13
+ */
+class Solution {
+    /**
+     * Definition for a Node.
+     */
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node left, Node right, Node next) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 方法一：层序遍历
+     */
+    public Node connect1(Node root) {
+        if (root == null) {
+            return null;
+        }
+        // Initialize a queue data structure which contains just the root of the tree
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        // Outer while loop which iterates over each level
+        while (queue.size() > 0) {
+            // Note the size of the queue
+            int size = queue.size();
+            // Iterate over all the nodes on the current level
+            for (int i = 0; i < size; ++i) {
+                // Pop a node from the front of the queue
+                Node node = queue.poll();
+                // This check is important. We don't want to establish any wrong connections.
+                // The queue will contain nodes from 2 levels at most at any point in time.
+                // This check ensures we only don't establish next pointers beyond the end of a level
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+                // Add the children, if any, to the back of the queue
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        // Since the tree has now been modified, return the root node
+        return root;
+    }
+
+    /**
+     * 方法二：使用已建立的 next 指针
+     */
+    public Node connect2(Node root) {
+        if (root == null) {
+            return null;
+        }
+        // 从根节点开始，由于第 0 层只有这一个节点，所以不需要连接
+        Node leftmost = root;
+        // 如果当前最左节点的左孩子不存在，说明已经到达该树的最后一层，完成了所有节点的连接
+        while (leftmost.left != null) {
+            // 遍历某一层的节点时，这层节点的 next 指针已经建立
+            // 因此我们只需要知道这一层的最左节点，就可以按照链表方式遍历，不需要使用队列
+            Node head = leftmost;
+            while (head != null) {
+                // CONNECTION 1：
+                // 两个子节点属于同一个父节点，因此直接通过父节点建立两个子节点的 next 指针即可
+                head.left.next = head.right;
+                // CONNECTION 2：
+                // 连接不同父节点之间的子节点。更具体地说，连接的是第一个父节点的右孩子和第二父节点的左孩子
+                // 由于已经在父节点这一层建立了 next 指针，因此可以直接通过第一个父节点的 next 指针找到第二个父节点，然后在它们的孩子之间建立连接
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+                // Progress along the list (nodes on the current level)
+                head = head.next;
+            }
+            // 完成当前层的连接后，进入下一层重复操作，直到所有的节点全部连接
+            // 进入下一层后需要更新最左节点，然后从新的最左节点开始遍历该层所有节点
+            // 因为是完美二叉树，因此最左节点一定是当前层最左节点的左孩子
+            leftmost = leftmost.left;
+        }
+        return root;
+    }
+
+    /**
+     * 方法三：使用已建立的 next 指针（递归法）
+     */
+    public Node connect3(Node root) {
+        return helper(root);
+    }
+
+    private Node helper(Node root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left != null) {
+            root.left.next = root.right;
+        }
+        if (root.right != null) {
+            root.right.next = root.next == null ? null : root.next.left;
+        }
+        helper(root.left);
+        helper(root.right);
+        return root;
+    }
+}
---
+++ src/com/scuyjzh/leetcode/medium/No_0117_Populating_Next_Right_Pointers_in_Each_Node_II/Solution.java
@@ -0,0 +1,104 @@
+package com.scuyjzh.leetcode.medium.No_0117_Populating_Next_Right_Pointers_in_Each_Node_II;
+
+import java.util.*;
+
+/**
+ * 117. 填充每个节点的下一个右侧节点指针 II
+ * 给定一个二叉树，填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ *
+ * @author scuyjzh
+ * @date 2020/9/28 12:15
+ */
+class Solution {
+    /**
+     * Definition for a Node.
+     */
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node left, Node right, Node next) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 方法一：层序遍历
+     */
+    public Node connect1(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            Node last = null;
+            for (int i = 1; i <= n; ++i) {
+                Node node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                if (i != 1) {
+                    last.next = node;
+                }
+                last = node;
+            }
+        }
+        return root;
+    }
+
+    /**
+     * 方法二：使用已建立的 next 指针
+     */
+    public Node connect2(Node root) {
+        if (root == null) {
+            return null;
+        }
+        // cur 我们可以把它看做是每一层的链表
+        Node cur = root;
+        while (cur != null) {
+            // 遍历当前层的时候，为了方便操作在下一层前面添加一个哑结点
+            // （注意这里是访问当前层的节点，然后把下一层的节点串联成链表）
+            Node dummy = new Node(0);
+            // pre 表示访下一层节点的前一个节点
+            Node pre = dummy;
+            // 然后开始遍历当前层的链表
+            while (cur != null) {
+                if (cur.left != null) {
+                    // 如果当前节点的左子节点不为空，就让 pre 节点的 next 指向它
+                    pre.next = cur.left;
+                    // 然后再更新 pre
+                    pre = pre.next;
+                }
+                // 同理参照左子树
+                if (cur.right != null) {
+                    pre.next = cur.right;
+                    pre = pre.next;
+                }
+                // 继续访问这一行的下一个节点
+                cur = cur.next;
+            }
+            // 把下一层串联成一个链表之后，让它赋值给 cur，
+            // 后续继续循环，直到 cur 为空为止
+            cur = dummy.next;
+        }
+        return root;
+    }
+}
