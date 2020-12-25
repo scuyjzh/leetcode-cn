dea5410d1eba1320187e2cf2967e0ea8c303a53c
scuyjzh <scuyjzh@163.com>
2020-12-25 10:29:29 +0800

    update

--- src/com/scuyjzh/leetcode/medium/No_0003_Longest_Substring_Without_Repeating_Characters/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0003_Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -5,21 +5,26 @@ import java.util.*;
 /**
  * 3. 无重复字符的最长子串
  * 给定一个字符串，请你找出其中不含有重复字符的最长子串的长度。
+ * 
  * 示例：
  * 输入: s = "abcabcbb"
  * 输出: 3
- * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
  */
 class Solution {
+    /**
+     * 方法：滑动窗口
+     * 时间复杂度：O(N)，其中 N 是字符串的长度。左指针和右指针分别会遍历整个字符串一次。
+     * 空间复杂度：O(|Σ|)，其中 Σ 表示字符集（即字符串中可以出现的字符），|Σ| 表示字符集的大小。
+     */
     public int lengthOfLongestSubstring(String s) {
         // 哈希集合，记录每个字符是否出现过
         Set<Character> occ = new HashSet<>();
         int n = s.length();
-        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        // 右指针rk，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
         int rk = -1, ans = 0;
         for (int i = 0; i < n; ++i) {
             if (i != 0) {
-                // 左指针向右移动一格，移除一个字符
+                // 左指针i向右移动一格，移除一个字符
                 occ.remove(s.charAt(i - 1));
             }
             while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
@@ -35,6 +40,6 @@ class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.lengthOfLongestSubstring("Characters"));
+        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
     }
 }
