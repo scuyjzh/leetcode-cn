0f1f3191b0d0b38ad9837f4b36be7abe30e1dd39
scuyjzh <scuyjzh@163.com>
2021-06-30 23:59:20 +0800

    update

--- src/com/scuyjzh/leetcode/medium/No_0002_Add_Two_Numbers/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0002_Add_Two_Numbers/Solution.java
@@ -7,7 +7,7 @@ import com.scuyjzh.leetcode.structure.ListNode;
  * 给出两个非空的链表用来表示两个非负的整数。其中，它们各自的位数是按照逆序的方式存储的，并且它们的每个节点只能存储一位数字。
  * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
  * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
- * 
+ * <p>
  * 示例：
  * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
  * 输出：7 -> 0 -> 8
@@ -16,11 +16,12 @@ import com.scuyjzh.leetcode.structure.ListNode;
 class Solution {
     /**
      * 方法一：模拟
-     * 时间复杂度：O(max(m,n))，其中 m,n 为两个链表的长度。我们要遍历两个链表的全部位置，而处理每个位置只需要 O(1) 的时间。
+     * 时间复杂度：O(max(m,n))，其中m、n为两个链表的长度。我们要遍历两个链表的全部位置，而处理每个位置只需要O(1)的时间。
      * 空间复杂度：O(max(m,n))。答案链表的长度最多为较长链表的长度+1。
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = null, tail = null;
+        // 设定进位值为carry
         int carry = 0;
         while (l1 != null || l2 != null) {
             // 同时遍历两个链表
