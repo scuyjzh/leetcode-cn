3b9e0fa24c5cbf4905df996f839a733baf701f84
scuyjzh <scuyjzh@163.com>
2021-06-30 23:42:16 +0800

    update

--- src/com/scuyjzh/leetcode/easy/No_0001_Two_Sum/Solution.java
+++ src/com/scuyjzh/leetcode/easy/No_0001_Two_Sum/Solution.java
@@ -6,7 +6,7 @@ import java.util.*;
  * 1. 两数之和
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- * 
+ * <p>
  * 示例：
  * 给定 nums = [2, 7, 11, 15], target = 9
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
@@ -19,15 +19,15 @@ class Solution {
      * 空间复杂度：O(1)。
      */
     public int[] twoSum1(int[] nums, int target) {
-        int n = nums.length;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
+        int len = nums.length;
+        for (int i = 0; i < len - 1; ++i) {
+            for (int j = i + 1; j < len; ++j) {
                 if (nums[j] + nums[i] == target) {
                     return new int[]{i, j};
                 }
             }
         }
-        return new int[0];
+        throw new IllegalArgumentException("No two sum solution");
     }
 
     /**
@@ -43,12 +43,12 @@ class Solution {
             }
             hashtable.put(nums[i], i);
         }
-        return new int[0];
+        throw new IllegalArgumentException("No two sum solution");
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(Arrays.toString(solution.twoSum1(new int[]{3, 2, 4}, 5)));
-        System.out.println(Arrays.toString(solution.twoSum2(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(solution.twoSum2(new int[]{3, 2, 4}, 8)));
     }
 }
