56e419b035332e2678fecd09794e5346b55ff7c3
scuyjzh <scuyjzh@163.com>
2021-07-04 04:27:11 +0800

    update

--- src/com/scuyjzh/leetcode/medium/No_0008_String_to_Integer/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0008_String_to_Integer/Solution.java
@@ -76,10 +76,10 @@ class Solution {
         /*
           正则表达式：^[+\\-]?\\d+（Java中需用双斜杠表示转义）
           ^   表示匹配字符串开头，此处匹配的就是 '+'、'-' 号
-          []  匹配方括号内的任意字符，比如[0-9]就是匹配数字字符 0 - 9 中的一个
+          []  匹配方括号内的任意字符，比如 [0-9] 就是匹配数字字符 0 - 9 中的一个
           ?   匹配之前的字符出现 >=0 次，这里用 ? 是因为 '+' 号可以省略
           \\d 匹配数字（0-9）
-          +   匹配之前的字符出现 >=1 次，\\d+匹配一串数字
+          +   匹配之前的字符出现 >=1 次，\\d+ 则匹配一串数字
          */
         Pattern p = Pattern.compile("^[+-]?\\d+");
         Matcher m = p.matcher(str);
@@ -97,6 +97,59 @@ class Solution {
         return value;
     }
 
+    /**
+     * 方法三：逐字匹配
+     */
+    public int myAtoi3(String str) {
+        int len = str.length();
+        // str.charAt(i) 方法回去检查下标的合法性，一般先转换成字符数组
+        char[] charArray = str.toCharArray();
+
+        // 1、去除前导空格
+        int index = 0;
+        while (index < len && charArray[index] == ' ') {
+            index++;
+        }
+
+        // 2、如果已经遍历完成（针对极端用例 "      "）
+        if (index == len) {
+            return 0;
+        }
+
+        // 3、如果出现符号字符，仅第 1 个有效，并记录正负
+        int sign = 1;
+        char firstChar = charArray[index];
+        if (firstChar == '+') {
+            index++;
+        } else if (firstChar == '-') {
+            index++;
+            sign = -1;
+        }
+
+        // 4、将后续出现的数字字符进行转换（题目规定不能使用 long 类型）
+        int res = 0;
+        while (index < len) {
+            char currChar = charArray[index];
+            // 4.1 先判断不合法的情况
+            if (currChar < '0' || currChar > '9') {
+                break;
+            }
+
+            // 题目规定环境只能存储 32 位大小的有符号整数，因此，需要提前判断乘以 10 以后是否越界
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && (currChar - '0') > Integer.MAX_VALUE % 10)) {
+                return Integer.MAX_VALUE;
+            }
+            if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && (currChar - '0') > -(Integer.MIN_VALUE % 10))) {
+                return Integer.MIN_VALUE;
+            }
+
+            // 4.2 合法的情况下，才考虑转换，每一步都把符号位乘进去
+            res = res * 10 + sign * (currChar - '0');
+            index++;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.myAtoi1("42"));
@@ -110,5 +163,11 @@ class Solution {
         System.out.println(solution.myAtoi2("4193 with words"));
         System.out.println(solution.myAtoi2("words and 987"));
         System.out.println(solution.myAtoi2("-91283472332"));
+
+        System.out.println(solution.myAtoi3("42"));
+        System.out.println(solution.myAtoi3("   -42"));
+        System.out.println(solution.myAtoi3("4193 with words"));
+        System.out.println(solution.myAtoi3("words and 987"));
+        System.out.println(solution.myAtoi3("-91283472332"));
     }
 }
