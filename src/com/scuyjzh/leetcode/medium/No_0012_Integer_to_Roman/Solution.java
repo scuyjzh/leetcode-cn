72114e2c66ded4bc83d67c6618e6e586a0211d1f
scuyjzh <scuyjzh@163.com>
2021-07-07 18:16:47 +0800

    update

--- src/com/scuyjzh/leetcode/medium/No_0012_Integer_to_Roman/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0012_Integer_to_Roman/Solution.java
@@ -12,14 +12,64 @@ package com.scuyjzh.leetcode.medium.No_0012_Integer_to_Roman;
  * D             500
  * M             1000
  * 例如，罗马数字 2 写做II，即为两个并列的 1。12 写做XII，即为X+II。27 写做XXVII, 即为XX+V+II。
- * <p>
  * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做IIII，而是IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为IX。
  * 这个特殊的规则只适用于以下六种情况：
  * - I可以放在V(5) 和X(10) 的左边，来表示 4 和 9。
  * - X可以放在L(50) 和C(100) 的左边，来表示 40 和90。
  * - C可以放在D(500) 和M(1000) 的左边，来表示400 和900。
- * <p>
  * 给你一个整数，将其转为罗马数字。
+ * <p>
+ * 提示：1 <= num <= 3999
  */
 class Solution {
+    /**
+     * 方法一：模拟
+     * 时间复杂度：O(1)。由于 valueSymbols 长度是固定的，且这 13 字符中的每个字符的出现次数均不会超过 3，因此循环次数有一个确定的上限。对于本题给出的数据范围，循环次数不会超过 15 次。
+     * 空间复杂度：O(1)。
+     */
+    public String intToRoman1(int num) {
+        // 把阿拉伯数字与罗马数字可能出现的所有情况和对应关系，放在两个数组中，并且按照阿拉伯数字的大小降序排列
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < values.length; ++i) {
+            int value = values[i];
+            String symbol = symbols[i];
+            // 特别注意：这里是等号
+            while (num >= value) {
+                num -= value;
+                roman.append(symbol);
+            }
+            if (num == 0) {
+                break;
+            }
+        }
+        return roman.toString();
+    }
+
+    /**
+     * 方法二：硬编码数字
+     * 时间复杂度：O(1)。计算量与输入数字的大小无关。
+     * 空间复杂度：O(1)。
+     */
+    public String intToRoman2(int num) {
+        String[] thousands = {"", "M", "MM", "MMM"};
+        String[] hundreds  = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] tens      = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] ones      = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        StringBuffer roman = new StringBuffer();
+        roman.append(thousands[num / 1000]);
+        roman.append(hundreds[num % 1000 / 100]);
+        roman.append(tens[num % 100 / 10]);
+        roman.append(ones[num % 10]);
+        return roman.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.intToRoman1(999));
+        System.out.println(solution.intToRoman2(999));
+    }
 }
