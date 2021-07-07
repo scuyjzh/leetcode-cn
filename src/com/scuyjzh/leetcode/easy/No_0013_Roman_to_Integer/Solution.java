b9e95bb4385ada69408c1e394b7e436206bfc168
scuyjzh <scuyjzh@163.com>
2021-07-07 18:40:34 +0800

    update

--- src/com/scuyjzh/leetcode/easy/No_0013_Roman_to_Integer/Solution.java
+++ src/com/scuyjzh/leetcode/easy/No_0013_Roman_to_Integer/Solution.java
@@ -21,4 +21,54 @@ package com.scuyjzh.leetcode.easy.No_0013_Roman_to_Integer;
  * 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
  */
 class Solution {
+    /**
+     * 方法一：模拟
+     * 时间复杂度：O(n)，其中 n 是字符串 s 的长度。
+     * 空间复杂度：O(1)。
+     */
+    public int romanToInt(String s) {
+        int sum = 0;
+        int preNum = getValue(s.charAt(0));
+        // 通常情况下，罗马数字中小的数字在大的数字的右边
+        // 若输入的字符串满足该情况，那么可以将每个字符视作一个单独的值，累加每个字符对应的数值即可
+        for (int i = 1; i < s.length(); i++) {
+            int num = getValue(s.charAt(i));
+            if (preNum < num) {
+                // 若存在小的数字在大的数字的左边的情况，根据规则需要减去小的数字
+                sum -= preNum;
+            } else {
+                sum += preNum;
+            }
+            preNum = num;
+        }
+        sum += preNum;
+        return sum;
+    }
+
+    private int getValue(char ch) {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.romanToInt("XIV"));
+        System.out.println(solution.romanToInt("XXVII"));
+    }
 }
