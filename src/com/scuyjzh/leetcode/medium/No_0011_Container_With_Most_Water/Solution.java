f4d85ae1a4747645e5608c3e32554c1f94a1c1e3
scuyjzh <scuyjzh@163.com>
2021-08-01 17:24:28 +0800

    modify solutions to problem - "11. Container With Most Water"

--- src/com/scuyjzh/leetcode/medium/No_0011_Container_With_Most_Water/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0011_Container_With_Most_Water/Solution.java
@@ -5,15 +5,18 @@ package com.scuyjzh.leetcode.medium.No_0011_Container_With_Most_Water;
  * <p>
  * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点(i,ai) 。在坐标内画 n 条垂直线，垂直线 i的两个端点分别为(i,ai) 和 (i, 0) 。
  * 找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。
- * 说明：你不能倾斜容器。
  */
 class Solution {
     /**
-     * 方法一：双指针
+     * 方法：双指针
      * 时间复杂度：O(N)，双指针总计最多遍历整个数组一次。
      * 空间复杂度：O(1)，只需要额外的常数级别的空间。
      */
     public int maxArea(int[] height) {
+        /*
+         * 思路：
+         * https://leetcode-cn.com/problems/container-with-most-water/solution/container-with-most-water-shuang-zhi-zhen-fa-yi-do/
+         */
         int l = 0, r = height.length - 1, res = 0;
         while (l < r) {
             res = height[l] < height[r] ?
