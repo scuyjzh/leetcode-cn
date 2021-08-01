89a0a2978e193cc3a3d4082b0d114f86b33a1d02
scuyjzh <scuyjzh@163.com>
2021-08-01 18:43:48 +0800

    modify solutions to problem - "33. Search in Rotated Sorted Array"

--- src/com/scuyjzh/leetcode/medium/No_0033_Search_in_Rotated_Sorted_Array/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0033_Search_in_Rotated_Sorted_Array/Solution.java
@@ -9,14 +9,19 @@ package com.scuyjzh.leetcode.medium.No_0033_Search_in_Rotated_Sorted_Array;
  * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回-1。
  */
 class Solution {
+    /**
+     * 方法：二分查找
+     * 时间复杂度：O(log n)，其中 n 为 nums 数组的大小。整个算法时间复杂度即为二分查找的时间复杂度 O(log n)。
+     * 空间复杂度：O(1) 。只需要常数级别的空间存放变量。
+     */
     public int search(int[] nums, int target) {
         /*
          * 思路：
          * 可以发现，将数组从中间分开成左右两部分的时候，一定有一部分的数组是有序的。
          * 因此可以在常规二分查找的时候查看当前 mid 为分割位置分割出来的两个部分 [l, mid] 和 [mid + 1, r] 哪个部分是有序的，
          * 并根据有序的那个部分确定该如何改变二分查找的上下界，因为根据有序的那部分能够判断出 target 在不在这个部分：
-         * - 如果 [l, mid - 1] 是有序数组，且 target 的大小满足 [nums[l],nums[mid])，则我们应该将搜索范围缩小至 [l, mid - 1]，否则在 [mid + 1, r] 中寻找。
-         * - 如果 [mid, r] 是有序数组，且 target 的大小满足 (nums[mid+1],nums[r]]，则我们应该将搜索范围缩小至 [mid + 1, r]，否则在 [l, mid - 1] 中寻找。
+         *   • 如果 [l, mid - 1] 是有序数组，且 target 的大小满足 [nums[l],nums[mid])，则应该将搜索范围缩小至 [l, mid - 1]，否则在 [mid + 1, r] 中寻找。
+         *   • 如果 [mid, r] 是有序数组，且 target 的大小满足 (nums[mid+1],nums[r]]，则应该将搜索范围缩小至 [mid + 1, r]，否则在 [l, mid - 1] 中寻找。
          */
         int len = nums.length;
         if (len == 0) {
