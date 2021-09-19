4d219b0c83b0cd334ef4f90f691d1f3311f79b85
scuyjzh <scuyjzh@163.com>
2021-09-19 23:53:49 +0800

    add solutions to problem - "154. Find Minimum in Rotated Sorted Array II"

--- README.md
+++ README.md
@@ -100,6 +100,7 @@
 | 33   | [搜索旋转排序数组](https://leetcode-cn.com/problems/search-in-rotated-sorted-array/) | [Java](./src/com/scuyjzh/leetcode/medium/No_0033_Search_in_Rotated_Sorted_Array/Solution.java) | 中等 | 数组 二分查找                              |
 | 81   | [搜索旋转排序数组 II](https://leetcode-cn.com/problems/search-in-rotated-sorted-array-ii/) | [Java](./src/com/scuyjzh/leetcode/medium/No_0081_Search_in_Rotated_Sorted_Array_II/Solution.java) | 中等 | 数组 二分查找                              |
 | 153  | [寻找旋转排序数组中的最小值](https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/) | [Java](./src/com/scuyjzh/leetcode/medium/No_0153_Find_Minimum_in_Rotated_Sorted_Array/Solution.java) | 中等 | 数组 二分查找                              |
+| 154  | [寻找旋转排序数组中的最小值 II](https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/) | [Java](./src/com/scuyjzh/leetcode/hard/No_0154_Find_Minimum_in_Rotated_Sorted_Array_II/Solution.java) | 困难 | 数组 二分查找                              |
 
 ### String
 
@@ -159,6 +160,7 @@
 | 169  | [多数元素](https://leetcode-cn.com/problems/majority-element/) | [Java](./src/com/scuyjzh/leetcode/easy/No_0169_Majority_Element/Solution.java) | 简单 | 数组 哈希表 分治 计数 排序                           |
 | 164  | [最大间距](https://leetcode-cn.com/problems/maximum-gap/)    | [Java](./src/com/scuyjzh/leetcode/hard/No_0164_Maximum_Gap/Solution.java) | 困难 | 数组 桶排序 基数排序 排序                            |
 | 163  | [缺失的区间](https://leetcode-cn.com/problems/missing-ranges/) 🔒 | [Java](./src/com/scuyjzh/leetcode/easy/No_0163_Missing_Ranges/Solution.java) | 简单 | 数组                                                 |
+| 154  | [寻找旋转排序数组中的最小值 II](https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/) | [Java](./src/com/scuyjzh/leetcode/hard/No_0154_Find_Minimum_in_Rotated_Sorted_Array_II/Solution.java) | 困难 | 数组 二分查找                                        |
 | 153  | [寻找旋转排序数组中的最小值](https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/) | [Java](./src/com/scuyjzh/leetcode/medium/No_0153_Find_Minimum_in_Rotated_Sorted_Array/Solution.java) | 中等 | 数组 二分查找                                        |
 | 152  | [乘积最大子数组](https://leetcode-cn.com/problems/maximum-product-subarray/) | [Java](./src/com/scuyjzh/leetcode/medium/No_0152_Maximum_Product_Subarray/Solution.java) | 中等 | 数组 动态规划                                        |
 | 142  | [环形链表 II](https://leetcode-cn.com/problems/linked-list-cycle-ii/) | [Java](./src/com/scuyjzh/leetcode/medium/No_0142_Linked_List_Cycle_II/Solution.java) | 中等 | 哈希表 链表 双指针                                   |
---
+++ src/com/scuyjzh/leetcode/hard/No_0154_Find_Minimum_in_Rotated_Sorted_Array_II/Solution.java
@@ -0,0 +1,92 @@
+package com.scuyjzh.leetcode.hard.No_0154_Find_Minimum_in_Rotated_Sorted_Array_II;
+
+/**
+ * 154. 寻找旋转排序数组中的最小值 II
+ * <p>
+ * 已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。
+ * 例如，原数组 nums = [0,1,4,4,5,6,7] 在变化后可能得到：
+ * • 若旋转 4 次，则可以得到 [4,5,6,7,0,1,4]
+ * • 若旋转 7 次，则可以得到 [0,1,4,4,5,6,7]
+ * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+ * 给你一个可能存在 重复 元素值的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。
+ * 请你找出并返回数组中的 最小元素 。
+ */
+class Solution {
+    /**
+     * 方法：二分查找
+     * 时间复杂度：平均时间复杂度为 O(logN)，其中 N 是数组 nums 的长度。如果数组是随机生成的，那么数组中包含相同元素的概率很低，在二分查找的过程中，大部分情况都会忽略一半的区间。而在最坏情况下，如果数组中的元素完全相同，那么 while 循环就需要执行 N 次，每次忽略区间的右端点，时间复杂度为 O(N)。
+     * 空间复杂度：O(1)。
+     */
+    public int findMin(int[] nums) {
+        /*
+         * 二分查找：
+         *   1.通过下标即可得到关键字
+         *   2.任取一个关键字的值即可确定所寻找关键字是在它前面还是后面
+         *
+         * 思路与算法：
+         * 考虑数组中的最后一个元素 x：
+         * 在最小值右侧的元素，它们的值一定都小于等于 x；而在最小值左侧的元素，它们的值一定都大于等于 x。
+         * 因此，可以根据这一条性质，通过二分查找的方法找出最小值。
+         *
+         * 在二分查找的每一步中，左边界为 left，右边界为 right，区间的中点为 mid，最小值就在该区间内。
+         * 将中轴元素 nums[mid] 与右边界元素 nums[right] 进行比较，可能会有以下的三种情况：
+         *   1.第一种情况是 nums[mid] < nums[right]。
+         *     这说明 nums[mid] 是最小值右侧的元素，因此可以忽略二分查找区间的右半部分。
+         *   2.第二种情况是 nums[mid] > nums[right]。
+         *     这说明 nums[mid] 是最小值左侧的元素，因此可以忽略二分查找区间的左半部分。
+         *   3.第三种情况是 nums[mid] = nums[right]。
+         *     由于重复元素的存在，并不能确定 nums[mid] 究竟在最小值的左侧还是右侧，因此不能莽撞地忽略某一部分的元素。
+         *     唯一可以知道的是，由于它们的值相同，所以无论 nums[right] 是不是最小值，都有一个它的「替代品」nums[mid]，
+         *     因此可以忽略二分查找区间的右端点。
+         *
+         * 当二分查找结束时，就得到了最小值所在的位置。
+         *
+         * 三个问题：
+         * 一、为什么 right-- 不会对结果产生影响？
+         *   • 产生影响条件：删除的元素为唯一最小元素
+         *   • 执行条件：nums[mid] == nums[right]
+         *   • 矛盾
+         *
+         * 二、为什么 right = mid 而 left = mid + 1？
+         *   • 一般二分查找：left = mid + 1 right = mid + 1
+         *     原因：确定 mid 所指元素并非查找元素
+         *   • nums[mid] > nums[right] 时，mid 所指一定不是最小值，因为一定比 nums[right] 大，所以 left = mid + 1；
+         *   • nums[mid] < nums[right] 时，mid 所指可能是最小值，所以 right = mid；
+         *
+         * 三、时间/空间复杂度是多少？
+         *   • 时间复杂度
+         *     - 一般情况：与二分相同为 O(logN)
+         *     - 特殊情况：始终执行 right--，复杂度为 O(N)
+         *   • 空间复杂度
+         *     - 指针：left right mid，复杂度 O(1)
+         */
+        int left = 0;
+        int right = nums.length - 1;
+
+        if (right == 0) {
+            return nums[0];
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            // 如果 nums[mid] < nums[right]，后半部分一定顺序，分界点在 mid 左边
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            }
+            // 如果 nums[mid] > nums[right]，前半部分一定顺序，分界点在 mid 右边
+            else if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            }
+            // 如果 nums[mid] == nums[right]，无法二分，暴力缩小范围
+            else if (nums[mid] == nums[right]) {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findMin(new int[]{1, 3, 5}));
+        System.out.println(new Solution().findMin(new int[]{2, 2, 2, 0, 1}));
+    }
+}
