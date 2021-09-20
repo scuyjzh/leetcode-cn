dce15fc2350e2a9116c6feb1cb6862e9faa1cf63
scuyjzh <scuyjzh@163.com>
2021-09-20 12:10:49 +0800

    modify solutions to problem - "34. Find First and Last Position of Element in Sorted Array"

--- README.md
+++ README.md
@@ -103,6 +103,7 @@
 | 154  | [寻找旋转排序数组中的最小值 II](https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/) | [Java](./src/com/scuyjzh/leetcode/hard/No_0154_Find_Minimum_in_Rotated_Sorted_Array_II/Solution.java) | 困难 | 数组 二分查找                              |
 | 162  | [寻找峰值](https://leetcode-cn.com/problems/find-peak-element/) | [Java](./src/com/scuyjzh/leetcode/medium/No_0162_Find_Peak_Element/Solution.java) | 中等 | 数组 二分查找                              |
 | 374  | [猜数字大小](https://leetcode-cn.com/problems/guess-number-higher-or-lower/) | [Java](https://leetcode-cn.com/problems/guess-number-higher-or-lower/solution/) | 简单 | 二分查找 交互                              |
+| 34   | [在排序数组中查找元素的第一个和最后一个位置](https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/) | [Java](./src/com/scuyjzh/leetcode/medium/No_0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.java) | 中等 | 数组 二分查找                              |
 
 ### String
 
--- src/com/scuyjzh/leetcode/medium/No_0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.java
@@ -5,7 +5,8 @@ import java.util.*;
 /**
  * 34. 在排序数组中查找元素的第一个和最后一个位置
  * <p>
- * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。
+ * 找出给定目标值在数组中的开始位置和结束位置。
  * 如果数组中不存在目标值 target，返回 [-1, -1]。
  * 进阶：你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
  */
@@ -17,27 +18,34 @@ class Solution {
      */
     public int[] searchRange(int[] nums, int target) {
         /*
-         * 思路：
+         * 直观的思路肯定是从前往后遍历一遍。
+         * 用两个变量记录第一次和最后一次遇见 target 的下标，但这个方法的时间复杂度为 O(n)，
+         * 没有利用到数组升序排列的条件。
+         *
          * 由于数组已经排序，因此整个数组是单调递增的，可以利用二分法来加速查找的过程。
-         * 考虑 target 开始和结束位置，其实要找的就是数组中「第一个等于 target 的位置」（记为 leftIdx）和「第一个大于 target 的位置减一」（记为 rightIdx）。
-         * 二分查找中，寻找 leftIdx 即为在数组中寻找第一个大于等于 target 的下标，寻找 rightIdx 即为在数组中寻找第一个大于 target 的下标，然后将下标减一。
-         * 最后，因为 target 可能不存在数组中，因此需要重新校验得到的两个下标 leftIdx 和 rightIdx。
+         *
+         * 考虑 target 开始和结束位置，其实要找的就是数组中「第一个等于 target 的位置」
+         * （记为 leftIdx）和「第一个大于 target 的位置减一」（记为 rightIdx）。
+         *
+         * 二分查找中，寻找 leftIdx 即为在数组中寻找第一个大于等于 target 的下标，
+         * 寻找 rightIdx 即为在数组中寻找第一个大于 target 的下标，然后将下标减一。
+         *
+         * 两者的判断条件不同，为了代码的复用，定义 binarySearch(nums, target, lower)
+         * 表示在 nums 数组中二分查找 target 的位置，如果 lower 为 true，则查找第一个
+         * 大于等于 target 的下标，否则查找第一个大于 target 的下标。
+         *
+         * 最后，因为 target 可能不存在数组中，因此需要重新校验得到的两个下标 leftIdx
+         * 和 rightIdx，看是否符合条件，如果符合条件就返回 [leftIdx,rightIdx]，不符
+         * 合就返回 [−1,−1]。
          */
         int leftIdx = binarySearch(nums, target, true);
         int rightIdx = binarySearch(nums, target, false) - 1;
-        // 校验
         if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
             return new int[]{leftIdx, rightIdx};
         }
         return new int[]{-1, -1};
     }
 
-    /**
-     * @param nums   查找数组
-     * @param target 查找目标值
-     * @param lower  标记位，如果值为 true，则查找第一个大于等于 target 的下标，否则查找第一个大于 target 的下标
-     * @return target 的位置
-     */
     private int binarySearch(int[] nums, int target, boolean lower) {
         int left = 0, right = nums.length - 1, ans = nums.length;
         while (left <= right) {
