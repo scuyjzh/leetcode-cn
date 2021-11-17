867e643c93ede75313627e1516b2a3155deaa2fb
scuyjzh <scuyjzh@163.com>
2021-11-18 00:37:00 +0800

    modify solutions to problem - "28. Implement strStr()"

--- src/com/scuyjzh/leetcode/easy/No_0028_strStr/Solution.java
+++ src/com/scuyjzh/leetcode/easy/No_0028_strStr/Solution.java
@@ -2,19 +2,19 @@ package com.scuyjzh.leetcode.easy.No_0028_strStr;
 
 /**
  * 28. 实现 strStr()
- * <p>
+ *
  * 实现strStr()函数。
- * 给你两个字符串haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。
- * 如果不存在，则返回 -1 。
- * 说明：
- * 当 needle 是空字符串时，应当返回什么值呢？这是一个在面试中很好的问题。
- * 对于本题而言，当 needle 是空字符串时应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
+ * 给你两个字符串haystack 和 needle ，请你在 haystack 字符串中找出
+ * needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返
+ * 回 -1 。
  */
 class Solution {
     /**
      * 方法一：暴力匹配
-     * 时间复杂度：O(n×m)，其中 n 是字符串 haystack 的长度，m 是字符串 needle 的长度。最坏情况下需要将字符串 needle 与字符串 haystack 的所有长度为 m 的子串均匹配一次。
-     * 空间复杂度：O(1)。只需要常数的空间保存若干变量。
+     *
+     * • 时间复杂度：O(n×m)，其中 n 是字符串 haystack 的长度，m 是字符串 needle 的长度。最坏情况下
+     *   需要将字符串 needle 与字符串 haystack 的所有长度为 m 的子串均匹配一次。
+     * • 空间复杂度：O(1)。只需要常数的空间保存若干变量。
      */
     public int strStr1(String haystack, String needle) {
         /*
@@ -25,9 +25,9 @@ class Solution {
          * 子串匹配成功，返回当前子串的开始位置即可。如果所有子串都匹配失败，则返回 −1。
          */
         int n = haystack.length(), m = needle.length();
-        for (int i = 0; i + m <= n; i++) {
+        for (int i = 0; i <= n - m; ++i) {
             boolean flag = true;
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < m; ++j) {
                 if (haystack.charAt(i + j) != needle.charAt(j)) {
                     flag = false;
                     break;
@@ -42,36 +42,56 @@ class Solution {
 
     /**
      * 方法二：Knuth-Morris-Pratt 算法
-     * 时间复杂度：O(n+m)，其中 n 是字符串 haystack 的长度，m 是字符串 needle 的长度。至多需要遍历两字符串一次。
-     * 空间复杂度：O(m)，其中 m 是字符串 needle 的长度。只需要保存字符串 needle 的前缀函数。
+     *
+     * • 时间复杂度：O(n+m)，其中 n 是字符串 haystack 的长度，m 是字符串 needle 的长度。至多需
+     *   要遍历两字符串一次。
+     * • 空间复杂度：O(m)，其中 m 是字符串 needle 的长度。只需要保存字符串 needle 的前缀函数。
      */
     public int strStr2(String haystack, String needle) {
         int n = haystack.length(), m = needle.length();
         if (m == 0) {
             return 0;
         }
-        int[] pi = new int[m];
-        for (int i = 1, j = 0; i < m; ++i) {
-            while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
-                j = pi[j - 1];
-            }
-            if (needle.charAt(i) == needle.charAt(j)) {
-                j++;
-            }
-            pi[i] = j;
-        }
+        // 构建 next 数组
+        int[] next = getNext(needle);
+
+        char[] strText = haystack.toCharArray();
+        char[] strKey = needle.toCharArray();
+
         for (int i = 0, j = 0; i < n; ++i) {
-            while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
-                j = pi[j - 1];
+            while (j > 0 && strText[i] != strKey[j]) {
+                j = next[j];
             }
-            if (haystack.charAt(i) == needle.charAt(j)) {
+            if (strText[i] == strKey[j]) {
                 j++;
             }
             if (j == m) {
-                return i - m + 1;
+                return i + 1 - j;
             }
         }
+
         return -1;
+    }
+
+    private int[] getNext(String ps) {
+        char[] strKey = ps.toCharArray();
+        int[] next = new int[strKey.length];
+
+        // 初始条件
+        int j = 0;
+        int k = -1;
+        next[0] = -1;
+
+        // 根据已知的前 j 位推测第 j+1 位
+        while (j < strKey.length - 1) {
+            if (k == -1 || strKey[j] == strKey[k]) {
+                next[++j] = ++k;
+            } else {
+                k = next[k];
+            }
+        }
+
+        return next;
     }
 
     /**
@@ -80,10 +100,10 @@ class Solution {
     public int strStr3(String haystack, String needle) {
         /*
          * 思想：
-         * Sunday算法由Daniel M.Sunday在1990年提出，它的思想跟BM算法很相似：
-         * 只不过Sunday算法是从前往后匹配，在匹配失败时关注的是主串中参加匹配的最末位字符的下一位字符。
+         * Sunday 算法由 Daniel M.Sunday 在 1990 年提出，它的思想跟 BM 算法很相似：
+         * 只不过 Sunday 算法是从前往后匹配，在匹配失败时关注的是主串中参加匹配的最末位字符的下一位字符。
          *   • 如果该字符没有在模式串中出现则直接跳过，即移动位数 = 模式串长度 + 1；
-         *   • 否则，其移动位数 = 模式串长度 - 该字符最右出现的位置(以0开始) = 模式串中该字符最右出现的位置到尾部的距离 + 1。
+         *   • 否则，其移动位数 = 模式串长度 - 该字符最右出现的位置(以 0 开始) = 模式串中该字符最右出现的位置到尾部的距离 + 1。
          */
         int hayLen = haystack.length();
         int nLen = needle.length();
