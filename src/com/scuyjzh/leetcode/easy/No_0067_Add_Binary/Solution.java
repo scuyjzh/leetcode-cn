50143c396f4eb4dff17f078bc07a3b6fb79d2bf8
scuyjzh <scuyjzh@163.com>
2021-11-24 01:27:08 +0800

    modify solutions to problem - "67. Add Binary"

--- src/com/scuyjzh/leetcode/easy/No_0067_Add_Binary/Solution.java
+++ src/com/scuyjzh/leetcode/easy/No_0067_Add_Binary/Solution.java
@@ -1,30 +1,48 @@
 package com.scuyjzh.leetcode.easy.No_0067_Add_Binary;
 
+/**
+ * 67. 二进制求和
+ *
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ */
 class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1, j = b.length() - 1, carry = 0;
-        while (i >= 0 || j >= 0) {
+        StringBuilder builder = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        // 循环相加两个字符串相同长度的低位数部分
+        while (i >= 0 && j >= 0) {
             int sum = carry;
-            if (i >= 0) {
-                sum += a.charAt(i--) - '0';
-            }
-            if (j >= 0) {
-                sum += b.charAt(j--) - '0';
-            }
-            sb.append(sum % 2);
+            sum += a.charAt(i--) - '0';
+            sum += b.charAt(j--) - '0';
             carry = sum / 2;
+            builder.append(sum % 2);
         }
-        if (carry != 0) {
-            sb.append(carry);
+        // 如果 a 还没遍历完成（a串比b串长），则继续遍历添加 a 的剩余部分
+        while (i >= 0) {
+            int sum = carry + a.charAt(i--) - '0';
+            carry = sum / 2;
+            builder.append(sum % 2);
         }
-        return sb.reverse().toString();
+        // 如果 b 还没遍历完成（b串比a串长），则继续遍历添加 b 的剩余部分
+        while (j >= 0) {
+            int sum = carry + b.charAt(j--) - '0';
+            carry = sum / 2;
+            builder.append(sum % 2);
+        }
+        // 如果 carry 不等于 0，还有个进位数没加进去，需要补充
+        if (carry == 1) {
+            builder.append(carry);
+        }
+        // 反转字符串获得正常结果
+        return builder.reverse().toString();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String a = "11111";
-        String b = "10111";
-        System.out.println(solution.addBinary(a, b));
+        System.out.println(solution.addBinary("11", "1"));
+        System.out.println(solution.addBinary("1010", "1011"));
     }
 }
