d13e98cfeef9d13f8ced0ae55f45febc00f17cc7
scuyjzh <scuyjzh@163.com>
2021-11-25 12:53:51 +0800

    modify solutions to problem - "16. 3Sum Closest"

--- README.md
+++ README.md
@@ -183,6 +183,7 @@
 | 1    | [两数之和](https://leetcode-cn.com/problems/two-sum/)        | [Java](./src/com/scuyjzh/leetcode/easy/No_0001_Two_Sum/Solution.java) | 简单 | 数组 哈希表                    |
 | 167  | [两数之和 II - 输入有序数组](https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/) | [Java](./src/com/scuyjzh/leetcode/easy/No_0167_Two_Sum_II/Solution.java) | 简单 | 数组 双指针 二分查找           |
 | 15   | [三数之和](https://leetcode-cn.com/problems/3sum/)           | [Java](./src/com/scuyjzh/leetcode/medium/No_0015_3Sum/Solution.java) | 中等 | 数组 双指针 排序               |
+| 16   | [最接近的三数之和](https://leetcode-cn.com/problems/3sum-closest/) | [Java](./src/com/scuyjzh/leetcode/medium/No_0016_3Sum_Closest/Solution.java) | 中等 | 数组 双指针 排序               |
 
 ### Tree
 
--- src/com/scuyjzh/leetcode/medium/No_0016_3Sum_Closest/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0016_3Sum_Closest/Solution.java
@@ -4,22 +4,27 @@ import java.util.*;
 
 /**
  * 16. 最接近的三数之和
- * <p>
- * 给定一个包括n个整数的数组nums和一个目标值target。找出nums中的三个整数，使得它们的和与target最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ *
+ * 给你一个长度为 n 的整数数组nums和 一个目标值target。请你从
+ * nums 中选出三个整数，使它们的和与target最接近。
+ * 返回这三个数的和。
+ * 假定每组输入只存在恰好一个解。
  */
 class Solution {
     /**
      * 方法：排序 + 双指针
-     * 时间复杂度：O(N^2)，其中 N 是数组 nums 的长度。首先需要 O(NlogN) 的时间对数组进行排序，随后在枚举的过程中，使用一重循环 O(N) 枚举 a，双指针 O(N) 枚举 b 和 c，故一共是 O(N^2)。
-     * 空间复杂度：O(logN)。排序需要使用 O(logN) 的空间。
+     *
+     * • 时间复杂度：O(N^2)，其中 N 是数组 nums 的长度。首先需要 O(NlogN) 的时间对数组进行排
+     *   序，随后在枚举的过程中，使用一重循环 O(N) 枚举 a，双指针 O(N) 枚举 b 和 c，故一共是 O(N^2)。
+     * • 空间复杂度：O(logN)。排序需要使用 O(logN) 的空间。
      */
     public int threeSumClosest(int[] nums, int target) {
         // 排序
         Arrays.sort(nums);
         int diff = Integer.MAX_VALUE, res = target;
         for (int i = 0; i < nums.length - 2; i++) {
+            // 跳过重复元素
             if (i > 0 && nums[i] == nums[i - 1]) {
-                // 跳过重复元素
                 continue;
             }
             int left = i + 1, right = nums.length - 1;
@@ -54,7 +59,7 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.threeSumClosest(new int[]{0, 2, 1, -3}, 1));
+        System.out.println(new Solution().threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+        System.out.println(new Solution().threeSumClosest(new int[]{0, 0, 0}, 1));
     }
 }
