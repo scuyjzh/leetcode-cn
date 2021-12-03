0c1a08242871bfe54f3fe62b301210d3d2389b08
scuyjzh <scuyjzh@163.com>
2021-12-03 12:59:48 +0800

    add solutions to problem - "104. Maximum Depth of Binary Tree"

--- src/com/scuyjzh/leetcode/easy/No_0104_Maximum_Depth_of_Binary_Tree/Solution.java
+++ src/com/scuyjzh/leetcode/easy/No_0104_Maximum_Depth_of_Binary_Tree/Solution.java
@@ -4,37 +4,25 @@ import com.scuyjzh.leetcode.structure.TreeNode;
 
 import java.util.*;
 
+/**
+ * 104. 二叉树的最大深度
+ *
+ * 给定一个二叉树，找出其最大深度。
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 说明: 叶子节点是指没有子节点的节点。
+ */
 class Solution {
     /**
-     * Approach #1 (Iteration with Queue - BFS)
+     * 方法一：深度优先搜索
      */
     public int maxDepth1(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int depth = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size-- > 0) {
-                TreeNode cur = queue.remove();
-                if (cur.left != null) {
-                    queue.add(cur.left);
-                }
-                if (cur.right != null) {
-                    queue.add(cur.right);
-                }
-            }
-            depth++;
-        }
-        return depth;
-    }
-
-    /**
-     * Approach #2 (Recursion - DFS)
-     */
-    public int maxDepth2(TreeNode root) {
+        /*
+         * 如果知道了左子树和右子树的最大深度 l 和 r，那么该二叉树的最大深度即为
+         *         max(l,r)+1
+         * 而左子树和右子树的最大深度又可以以同样的方式进行计算。因此可以用「深度优先搜索」的方法来计
+         * 算二叉树的最大深度。具体而言，在计算当前二叉树的最大深度时，可以先递归计算出其左子树和右子树的
+         * 最大深度，然后在 O(1) 时间内计算出当前二叉树的最大深度。递归在访问到空节点时退出。
+         */
         return dfs(root);
     }
 
@@ -43,6 +31,39 @@ class Solution {
             return 0;
         }
         return 1 + Math.max(dfs(root.left), dfs(root.right));
+    }
+
+    /**
+     * 方法二：广度优先搜索
+     */
+    public int maxDepth2(TreeNode root) {
+        /*
+         * 也可以用「广度优先搜索」的方法来解决这道题目，但需要对其进行一些修改，此时广度优先
+         * 搜索的队列里存放的是「当前层的所有节点」。每次拓展下一层的时候，不同于广度优先搜索的每次只从队
+         * 列里拿出一个节点，需要将队列里的所有节点都拿出来进行拓展，这样能保证每次拓展完的时候队列里
+         * 存放的是当前层的所有节点，即是一层一层地进行拓展，最后用一个变量 ans 来维护拓展的次数，
+         * 该二叉树的最大深度即为 ans。
+         */
+        if (root == null) {
+            return 0;
+        }
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
     }
 
     public static void main(String[] args) {
