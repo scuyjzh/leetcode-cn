563e63e6b0e2b95510796c303cbaeaa58f528b70
scuyjzh <scuyjzh@163.com>
2021-12-07 02:35:10 +0800

    modify solutions to problem - "117. Populating Next Right Pointers in Each Node II"

--- src/com/scuyjzh/leetcode/medium/No_0117_Populating_Next_Right_Pointers_in_Each_Node_II/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0117_Populating_Next_Right_Pointers_in_Each_Node_II/Solution.java
@@ -1,10 +1,12 @@
 package com.scuyjzh.leetcode.medium.No_0117_Populating_Next_Right_Pointers_in_Each_Node_II;
 
+import com.scuyjzh.leetcode.structure.TreeNode;
+
 import java.util.*;
 
 /**
- * 117. 填充每个结点的下一个右侧结点指针 II
- * <p>
+ * 117. 填充每个节点的下一个右侧节点指针 II
+ *
  * 给定一个二叉树
  *   struct Node {
  *     int val;
@@ -12,8 +14,8 @@ import java.util.*;
  *     Node *right;
  *     Node *next;
  *   }
- * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。
- * 如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到
+ * 下一个右侧节点，则将 next 指针设置为 NULL。
  * 初始状态下，所有 next 指针都被设置为 NULL。
  * 进阶：
  *   • 你只能使用常量级额外空间。
@@ -21,64 +23,46 @@ import java.util.*;
  */
 class Solution {
     /**
-     * Definition for a Node.
-     */
-    class Node {
-        public int val;
-        public Node left;
-        public Node right;
-        public Node next;
-
-        public Node() {
-        }
-
-        public Node(int _val) {
-            val = _val;
-        }
-
-        public Node(int _val, Node _left, Node _right, Node _next) {
-            val = _val;
-            left = _left;
-            right = _right;
-            next = _next;
-        }
-    }
-
-    /**
      * 方法一：层序遍历
-     * 时间复杂度：O(N)。需要遍历这棵树上所有的点，时间复杂度为 O(N)。
-     * 空间复杂度：O(N)。即队列的空间代价。
+     *
+     * • 时间复杂度：O(N)。需要遍历这棵树上所有的点，时间复杂度为 O(N)。
+     * • 空间复杂度：O(N)。即队列的空间代价。
      */
-    public Node connect1(Node root) {
+    public TreeNode connect1(TreeNode root) {
         /*
-         * 思路：
          * 这道题希望把二叉树各个层的点组织成链表，一个非常直观的思路是层次遍历。树的层次遍历基于广度
          * 优先搜索，它按照层的顺序遍历二叉树，在遍历第 i 层前，一定会遍历完第 i−1 层。
          *
-         * 算法：
-         * 初始化一个队列 q，将根结点放入队列中。当队列不为空的时候，记录当前队列大小为 n，从队列中以
-         * 此取出 n 个元素并通过这 n 个元素拓展新节点。如此循环，直到队列为空。
+         * 算法如下：初始化一个队列 q，将根结点放入队列中。当队列不为空的时候，记录当前队列大小为 n，从队
+         * 列中以此取出 n 个元素并通过这 n 个元素拓展新节点。如此循环，直到队列为空。
          *
-         * 这样做可以保证每次遍历的 n 个点都是同一层的。可以在遍历每一层的时候修改这一层节点的 next
-         * 指针，这样就可以把每一层都组织成链表。
+         * 这样做可以保证每次遍历的 n 个点都是同一层的。可以在遍历每一层的时候修改这一层节点的 next 指
+         * 针，这样就可以把每一层都组织成链表。
          */
         if (root == null) {
             return null;
         }
-        Queue<Node> queue = new LinkedList<>();
+        // 初始化队列同时将第一层节点加入队列中，即根节点
+        Deque<TreeNode> queue = new ArrayDeque<>();
         queue.offer(root);
+        // 外层的 while 循环迭代的是层数
         while (!queue.isEmpty()) {
-            int n = queue.size();
-            Node last = null;
-            for (int i = 1; i <= n; ++i) {
-                Node node = queue.poll();
+            // 记录当前队列大小
+            int size = queue.size();
+            TreeNode last = null;
+            // 遍历这一层的所有节点
+            for (int i = 0; i < size; ++i) {
+                // 从队首取出元素
+                TreeNode node = queue.poll();
+                // 拓展下一层节点
                 if (node.left != null) {
                     queue.offer(node.left);
                 }
                 if (node.right != null) {
                     queue.offer(node.right);
                 }
-                if (i != 1) {
+                // 连接
+                if (i != 0) {
                     last.next = node;
                 }
                 last = node;
@@ -89,14 +73,14 @@ class Solution {
 
     /**
      * 方法二：使用已建立的 next 指针
-     * 时间复杂度：O(N)。分析同「方法一」。
-     * 空间复杂度：O(1)。
+     *
+     * • 时间复杂度：O(N)。分析同「方法一」。
+     * • 空间复杂度：O(1)。
      */
-    Node last = null, nextStart = null;
+    TreeNode last = null, nextStart = null;
 
-    public Node connect2(Node root) {
+    public TreeNode connect2(TreeNode root) {
         /*
-         * 思路与算法：
          * 因为必须处理树上的所有节点，所以无法降低时间复杂度，但是可以尝试降低空间复杂度。
          *
          * 在方法一中，因为对树的结构一无所知，所以使用队列保证有序访问同一层的所有节点，并建立它们之间的
@@ -117,11 +101,11 @@ class Solution {
         if (root == null) {
             return null;
         }
-        Node start = root;
+        TreeNode start = root;
         while (start != null) {
             last = null;
             nextStart = null;
-            for (Node p = start; p != null; p = p.next) {
+            for (TreeNode p = start; p != null; p = p.next) {
                 if (p.left != null) {
                     handle(p.left);
                 }
@@ -134,7 +118,7 @@ class Solution {
         return root;
     }
 
-    private void handle(Node p) {
+    private void handle(TreeNode p) {
         if (last != null) {
             last.next = p;
         }
@@ -142,5 +126,10 @@ class Solution {
             nextStart = p;
         }
         last = p;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().connect1(TreeNode.initBinaryTree("[1,2,3,4,5,null,7]")));
+        System.out.println(new Solution().connect2(TreeNode.initBinaryTree("[1,2,3,4,5,null,7]")));
     }
 }
