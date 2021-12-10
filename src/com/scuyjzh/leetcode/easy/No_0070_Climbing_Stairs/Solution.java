8cd0a7d5b64d56c5143ffa3a150c41fb826f47d3
scuyjzh <scuyjzh@163.com>
2021-12-10 19:37:36 +0800

    add solutions to problem - "70. Climbing Stairs"

--- src/com/scuyjzh/leetcode/easy/No_0070_Climbing_Stairs/Solution.java
+++ src/com/scuyjzh/leetcode/easy/No_0070_Climbing_Stairs/Solution.java
@@ -1,26 +1,32 @@
 package com.scuyjzh.leetcode.easy.No_0070_Climbing_Stairs;
 
+/**
+ * 70. 爬楼梯
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ */
 class Solution {
-    /**
-     * 爬第n阶楼梯的方法数量，等于2部分之和:
-     * 1.爬上 n−1 阶楼梯的方法数量。因为再爬1阶就能到第n阶
-     * 2.爬上 n-2 阶楼梯的方法数量。因为再爬2阶就能到第n阶
-     */
     public int climbStairs(int n) {
         if (n <= 2) {
             return n;
         }
-        int[] f = new int[n + 1];
-        f[1] = 1;
-        f[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            f[i] = f[i - 1] + f[i - 2];
+
+        // 状态初始化
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; ++i) {
+            // 状态转移：f[n] = f[n−1] + f[n−2]
+            int third = first + second;
+            first = second;
+            second = third;
         }
-        return f[n];
+        return second;
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.climbStairs(3));
+        System.out.println(new Solution().climbStairs(2));
+        System.out.println(new Solution().climbStairs(3));
     }
 }
