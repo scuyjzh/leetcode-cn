90cbcd4d3957141649c5c3cf510040facda2b5cf
scuyjzh <scuyjzh@163.com>
2021-12-18 03:26:45 +0800

    modify solutions to problem - "53. Maximum Subarray"

--- src/com/scuyjzh/leetcode/easy/No_0053_Maximum_Subarray/Solution.java
+++ src/com/scuyjzh/leetcode/easy/No_0053_Maximum_Subarray/Solution.java
@@ -3,8 +3,8 @@ package com.scuyjzh.leetcode.easy.No_0053_Maximum_Subarray;
 /**
  * 53. 最大子数组和
  *
- * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包
- * 含一个元素），返回其最大和。
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数
+ * 组最少包含一个元素），返回其最大和。
  * 子数组 是数组中的一个连续部分。
  */
 class Solution {
@@ -15,20 +15,21 @@ class Solution {
         /*
          * 假设 nums 数组的长度是 n，下标从 0 到 n−1。
          * 用 f(i) 代表以第 i 个数结尾的「连续子数组的最大和」，那么很显然要求的答案就是：
-         *         max{f(0),f(1),f(2),...,f(n-1)}
+         *                          max{f(0),f(1),f(2),...,f(n-1)}
          *
          * 因此只需要求出每个位置的 f(i)，然后返回 f 数组中的最大值即可。那么如何求 f(i) 呢？可以
          * 考虑 nums[i] 单独成为一段还是加入 f(i−1) 对应的那一段，这取决于 nums[i] 和 f(i−1)+nums[i] 的大
          * 小，希望获得一个比较大的，于是可以写出这样的动态规划转移方程：
-         *         f(i)=max{f(i−1)+nums[i],nums[i]}
+         *                          f(i)=max{f(i−1)+nums[i],nums[i]}
          *
          * 不难给出一个时间复杂度 O(n)、空间复杂度 O(n) 的实现，即用一个 f 数组来保存 f(i) 的值，用一个循环
          * 求出所有 f(i)。
          */
-        int[] f = new int[nums.length];
+        int len = nums.length;
+        int[] f = new int[len];
         f[0] = nums[0];
         int res = nums[0];
-        for (int i = 1; i < nums.length; ++i) {
+        for (int i = 1; i < len; ++i) {
             f[i] = Math.max(f[i - 1] + nums[i], nums[i]);
             res = Math.max(res, f[i]);
         }
@@ -56,7 +57,7 @@ class Solution {
      */
     public int maxSubArray3(int[] nums) {
         // sum(i,j) = sum(0,j) - sum(0,i)
-        // 只要记录前 i 总和最小值就可以了
+        // 只要记录前 i 个数的总和最小值就可以了
         int allSum = 0;
         int minSum = 0;
         int res = Integer.MIN_VALUE;
