a7007308efc37d05222b23fde7ef351f5f995e5c
scuyjzh <scuyjzh@163.com>
2022-01-04 10:53:57 +0800

    modify solutions to problem - "56. Merge Intervals"

--- src/com/scuyjzh/leetcode/medium/No_0056_Merge_Intervals/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0056_Merge_Intervals/Solution.java
@@ -4,16 +4,12 @@ import java.util.*;
 
 /**
  * 56. 合并区间
- * <p>
- * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [start_i, end_i] 。
- * 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+ *
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为
+ * intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一
+ * 个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
  */
 class Solution {
-    /**
-     * 方法：排序
-     * 时间复杂度：O(NlogN)，其中 N 为区间的数量。除去排序的开销，只需要一次线性扫描，所以主要的时间开销是排序的 O(NlogN)。
-     * 空间复杂度：O(NlogN)，其中 N 为区间的数量。这里计算的是存储答案之外，使用的额外空间。O(logN) 即为排序所需要的空间复杂度。
-     */
     public int[][] merge(int[][] intervals) {
         /*
          * 思路：
@@ -21,21 +17,25 @@ class Solution {
          *
          * 算法：
          * 用数组 merged 存储最终的答案。
-         * 首先，将列表中的区间按照左端点升序排序。
-         * 然后将第一个区间加入 merged 数组中，并按顺序依次考虑之后的每个区间：
-         *   • 如果当前区间的左端点在数组 merged 中最后一个区间的右端点之后，那么它们不会重合，可以直接将这个区间加入数组 merged 的末尾；
-         *   • 否则，它们重合，需要用当前区间的右端点更新数组 merged 中最后一个区间的右端点，将其置为二者的较大值。
+         * 首先，将列表中的区间按照左端点升序排序。然后将第一个区间加入 merged 数组中，并按顺序依
+         * 次考虑之后的每个区间：
+         *   • 如果当前区间的左端点在数组 merged 中最后一个区间的右端点之后，那么它们不会重合，可以
+         *     直接将这个区间加入数组 merged 的末尾；
+         *   • 否则，它们重合，需要用当前区间的右端点更新数组 merged 中最后一个区间的右端点，将其置
+         *     为二者的较大值。
          */
         if (intervals.length == 0) {
             return new int[0][2];
         }
-        // 按照区间的左端点排序
-        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
         // 用数组 merged 存储最终的答案
         List<int[]> merged = new ArrayList<>();
+        // 首先，将列表中的区间按照左端点升序排序
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
         for (int[] interval : intervals) {
             int left = interval[0], right = interval[1];
-            // 如果当前区间的左端点在数组 merged 中最后一个区间的右端点之后，那么它们不会重合，可以直接将这个区间加入数组 merged 的末尾
+            // 然后将第一个区间加入 merged 数组中，并按顺序依次考虑之后的每个区间：
+            // 如果当前区间的左端点在数组 merged 中最后一个区间的右端点之后，那么它们不会重合，
+            // 可以直接将这个区间加入数组 merged 的末尾
             if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < left) {
                 merged.add(new int[]{left, right});
             }
