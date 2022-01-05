6bc612fd2a3a31b7571d48e5a9c7726f3796eca6
scuyjzh <scuyjzh@163.com>
2022-01-05 10:38:45 +0800

    modify solutions to problem - "253. Meeting Rooms II"

--- src/com/scuyjzh/leetcode/medium/No_0253_Meeting_Rooms_II/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0253_Meeting_Rooms_II/Solution.java
@@ -4,96 +4,82 @@ import java.util.*;
 
 /**
  * 253. 会议室 II
- * <p>
- * 给你一个会议时间安排的数组 intervals ，每个会议时间都会包括开始和结束的时间 intervals[i] = [start_i, end_i] ，
- * 为避免会议冲突，同时要考虑充分利用会议室资源，请你计算至少需要多少间会议室，才能满足这些会议安排。
+ *
+ * 给你一个会议时间安排的数组 intervals ，每个会议时间都会包括开始和
+ * 结束的时间 intervals[i] = [starti, endi] ，为避免会议冲突，同时要
+ * 考虑充分利用会议室资源，请你计算至少需要多少间会议室，才能满足这些
+ * 会议安排。
  */
 class Solution {
     /**
-     * 方法一：优先队列
-     * 时间复杂度：O(NlogN)。时间开销主要有两部分。第一部分是数组的 排序 过程，消耗 O(NlogN) 的时间。数组中有 N 个元素。接下来是 最小堆 占用的时间。在最坏的情况下，全部 N 个会议都会互相冲突。在任何情况下，都要向堆执行 N 次插入操作。在最坏的情况下，要对堆进行 N 次查找并删除最小值操作。总的时间复杂度为 (NlogN) ，因为查找并删除最小值操作只消耗 O(logN) 的时间。
-     * 空间复杂度：O(N) 。额外空间用于建立 最小堆 。在最坏的情况下，堆需要容纳全部 N 个元素。因此空间复杂度为 O(N) 。
+     * 方法一：优先队列（最小堆）
      */
     public int minMeetingRooms1(int[][] intervals) {
-
-        // Check for the base case. If there are no intervals, return 0
         if (intervals.length == 0) {
             return 0;
         }
 
-        // Min heap
-        PriorityQueue<Integer> allocator = new PriorityQueue<>(intervals.length, Comparator.comparingInt(a -> a));
-
-        // Sort the intervals by start time
+        // 1.按照 开始时间 对会议进行排序
         Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
 
-        // Add the first meeting
+        // 2.初始化一个新的 最小堆，将第一个会议的结束时间加入到堆中
+        PriorityQueue<Integer> allocator = new PriorityQueue<>(intervals.length, Comparator.comparingInt(a -> a));
         allocator.add(intervals[0][1]);
 
-        // Iterate over remaining intervals
+        // 3.对每个会议，检查堆的最小元素（即堆顶部的房间）是否空闲，它是最先开完会腾出房间的
         for (int i = 1; i < intervals.length; ++i) {
 
-            // If the room due to free up the earliest is free, assign that room to this meeting.
+            // 3.1.若房间空闲，则从堆顶拿出该元素，将其改为处理的会议的结束时间，加回到堆中
             if (intervals[i][0] >= allocator.peek()) {
                 allocator.poll();
             }
 
-            // If a new room is to be assigned, then also we add to the heap,
-            // If an old room is allocated, then also we have to add to the heap with updated end time.
+            // 3.2.若房间不空闲，开新房间，并加入到堆中
             allocator.add(intervals[i][1]);
         }
 
-        // The size of the heap tells us the minimum rooms required for all the meetings.
+        // 4.处理完所有会议后，堆的大小即为开的房间数量。这就是容纳这些会议需要的最小房间数
         return allocator.size();
     }
 
     /**
      * 方法二：有序化
-     * 时间复杂度: O(NlogN)。所做的只是将 开始时间 和 结束时间 两个数组分别进行排序。每个数组有 N 个元素，因为有 N 个时间间隔。
-     * 空间复杂度: O(N)。建立了两个 N 大小的数组。分别用于记录会议的开始时间和结束时间。
      */
     public int minMeetingRooms2(int[][] intervals) {
-
-        // Check for the base case. If there are no intervals, return 0
         if (intervals.length == 0) {
             return 0;
         }
 
+        // 1.分别将开始时间和结束时间存进两个数组
         Integer[] start = new Integer[intervals.length];
         Integer[] end = new Integer[intervals.length];
-
         for (int i = 0; i < intervals.length; ++i) {
             start[i] = intervals[i][0];
             end[i] = intervals[i][1];
         }
 
-        // Sort the intervals by end time
+        // 2.分别对开始时间和结束时间进行排序。请注意，这将打乱开始时间和结束时间的原始对应关系。它们将被分别处理
+        Arrays.sort(start, Comparator.comparingInt(a -> a));
         Arrays.sort(end, Comparator.comparingInt(a -> a));
 
-        // Sort the intervals by start time
-        Arrays.sort(start, Comparator.comparingInt(a -> a));
-
-        // The two pointers in the algorithm: e_ptr and s_ptr.
+        // 3.考虑两个指针：s_ptr 和 e_ptr ，分别代表开始指针和结束指针。开始指针遍历每个会议，结束指针帮助跟踪会议是否结束
         int startPointer = 0, endPointer = 0;
 
-        // Variables to keep track of maximum number of rooms used.
         int usedRooms = 0;
 
-        // Iterate over intervals.
         while (startPointer < intervals.length) {
 
-            // If there is a meeting that has ended by the time the meeting at `start_pointer` starts
+            // 4.当考虑 s_ptr 指向的特定会议时，检查该开始时间是否大于 e_ptr 指向的会议。
+            //   若如此，则说明 s_ptr 开始时，已经有会议结束。于是可以重用房间
             if (start[startPointer] >= end[endPointer]) {
-                usedRooms -= 1;
+                // 5.若有会议结束，换而言之，start[s_ptr] >= end[e_ptr] ，则自增 e_ptr
                 endPointer += 1;
+            } else {
+                // 否则，就需要开新房间
+                usedRooms += 1;
             }
-
-            // We do this irrespective of whether a room frees up or not.
-            // If a room got free, then this used_rooms += 1 wouldn't have any effect. used_rooms would
-            // remain the same in that case. If no room was free, then this would increase used_rooms
-            usedRooms += 1;
+            // 6.重复这一过程，直到 s_ptr 处理完所有会议
             startPointer += 1;
-
         }
 
         return usedRooms;
