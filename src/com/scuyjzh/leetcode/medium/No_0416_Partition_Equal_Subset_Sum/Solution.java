3106697603f1f82b0626e8a67efb63b53495a767
scuyjzh <scuyjzh@163.com>
2022-01-13 15:46:45 +0800

    modify solutions to problem - "416. Partition Equal Subset Sum"

--- src/com/scuyjzh/leetcode/medium/No_0416_Partition_Equal_Subset_Sum/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0416_Partition_Equal_Subset_Sum/Solution.java
@@ -7,114 +7,233 @@ package com.scuyjzh.leetcode.medium.No_0416_Partition_Equal_Subset_Sum;
  * 组分割成两个子集，使得两个子集的元素和相等。
  */
 class Solution {
-    /**
-     * 方法一：动态规划
-     */
     public boolean canPartition1(int[] nums) {
-        int len = nums.length;
-        // 题目已经说非空数组，可以不做非空判断
+        // 问题等效于「能否从数组中挑选若干个元素，使得其总和等于数组所有元素总和的一半」
+        // 抽象成「背包问题」：背包容量为 target=sum/2，每个数组元素的「价值」与「成本」都是其数值大小，求能否装满背包
+        int n = nums.length;
+
+        //「等和子集」的和必然是总和的一半
         int sum = 0;
         for (int num : nums) {
             sum += num;
         }
-        // 特判：如果是奇数，就不符合要求
-        if ((sum & 1) == 1) {
+        int target = sum / 2;
+
+        // 对应了总和为奇数的情况，注定不能被分为两个「等和子集」
+        if (target * 2 != sum) {
             return false;
         }
 
-        int target = sum / 2;
-        // 创建二维状态数组，行：物品索引，列：容量（包括 0）
-        boolean[][] dp = new boolean[len][target + 1];
+        // dp[i][j] 代表考虑前 i 个数值，其选择数字总和不超过 j 的最大价值
+        int[][] dp = new int[n][target + 1];
 
-        // 先填表格第 0 行，第 1 个数只能让容积为它自己的背包恰好装满
-        if (nums[0] <= target) {
-            dp[0][nums[0]] = true;
+        // 先处理考虑第 1 件物品（i=0）的情况
+        for (int j = 0; j <= target; j++) {
+            dp[0][j] = j >= nums[0] ? nums[0] : 0;
         }
-        // 再填表格后面几行
-        for (int i = 1; i < len; i++) {
-            for (int j = 0; j <= target; ++j) {
-                // 直接从上一行先把结果抄下来，然后再修正
-                dp[i][j] = dp[i - 1][j];
 
-                if (nums[i] == j) {
-                    dp[i][j] = true;
-                    continue;
-                }
-                if (nums[i] < j) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
-                }
+        // 再处理考虑其余物品（1≤i<n）的情况
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j <= target; ++j) {
+                // 不选第 i 件物品
+                int no = dp[i - 1][j];
+                // 选第 i 件物品
+                int yes = j >= nums[i] ? dp[i - 1][j - nums[i]] + nums[i] : 0;
+                dp[i][j] = Math.max(no, yes);
             }
         }
-        return dp[len - 1][target];
+        // 如果最大价值等于 target，说明可以拆分成两个「等和子集」
+        return dp[n - 1][target] == target;
     }
 
-    /**
-     * 方法二：动态规划 + 剪枝优化
-     */
     public boolean canPartition2(int[] nums) {
-        int len = nums.length;
+        // 「滚动数组」解法
+        int n = nums.length;
+
+        //「等和子集」的和必然是总和的一半
         int sum = 0;
         for (int num : nums) {
             sum += num;
         }
-        if ((sum & 1) == 1) {
+        int target = sum / 2;
+
+        // 对应了总和为奇数的情况，注定不能被分为两个「等和子集」
+        if (target * 2 != sum) {
             return false;
         }
 
-        int target = sum / 2;
-        boolean[][] dp = new boolean[len][target + 1];
+        // 将「物品维度」修改为 2
+        int[][] dp = new int[2][target + 1];
+        // 先处理考虑第 1 件物品（i=0）的情况
+        for (int j = 0; j <= target; ++j) {
+            dp[0][j] = j >= nums[0] ? nums[0] : 0;
+        }
 
-        // 初始化成为 true 虽然不符合状态定义，但是从状态转移来说是完全可以的
+        // 再处理考虑其余物品（1≤i<n）的情况
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j <= target; ++j) {
+                // 不选第 i 件物品，将物品维度的使用加上「&1」
+                int no = dp[(i - 1) & 1][j];
+                // 选第 i 件物品，将物品维度的使用加上「&1」
+                int yes = j >= nums[i] ? dp[(i - 1) & 1][j - nums[i]] + nums[i] : 0;
+                // 根据当前计算的行号是偶数还是奇数来交替使用第 0 行和第 1 行
+                dp[i & 1][j] = Math.max(no, yes);
+            }
+        }
+        // 如果最大价值等于 target，说明可以拆分成两个「等和子集」
+        // 将物品维度的使用加上「&1」
+        return dp[(n - 1) & 1][target] == target;
+    }
+
+    public boolean canPartition3(int[] nums) {
+        // 「一维空间优化」解法：
+        // 只保留代表「剩余容量」的维度，同时将容量遍历方向修改为「从大到小」
+        int n = nums.length;
+
+        //「等和子集」的和必然是总和的一半
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int target = sum / 2;
+
+        // 对应了总和为奇数的情况，注定不能被分为两个「等和子集」
+        if (target * 2 != sum) {
+            return false;
+        }
+
+        // 将「物品维度」取消，只保留代表「剩余容量」的维度
+        int[] dp = new int[target + 1];
+
+        // 先处理考虑第 1 件物品（i=0）的情况
+        for (int j = 0; j <= target; ++j) {
+            dp[j] = j >= nums[0] ? nums[0] : 0;
+        }
+
+        // 再处理考虑其余物品（1≤i<n）的情况
+        for (int i = 1; i < n; ++i) {
+            // 将容量遍历方向修改为「从大到小」
+            for (int j = target; j >= nums[i]; --j) {
+                // 不选第 i 件物品
+                int no = dp[j];
+                // 选第 i 件物品
+                int yes = dp[j - nums[i]] + nums[i];
+                dp[j] = Math.max(no, yes);
+            }
+        }
+        // 如果最大价值等于 target，说明可以拆分成两个「等和子集」
+        return dp[target] == target;
+    }
+
+    public boolean canPartition4(int[] nums) {
+        // 问题等效于「能否从数组中挑选若干个元素，使得元素总和等于所有元素总和的一半」
+        int n = nums.length;
+
+        //「等和子集」的和必然是总和的一半
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int target = sum / 2;
+
+        // 对应了总和为奇数的情况，注定不能被分为两个「等和子集」
+        if (target * 2 != sum) {
+            return false;
+        }
+
+        // dp[i][j] 表示从数组的 [0,i) 下标范围内选取若干个正整数（可以是 0 个），
+        // 是否存在一种选取方案使得被选取的正整数的和等于 j
+        boolean[][] dp = new boolean[n + 1][target + 1];
+
+        // 初始化一个「有效值」
         dp[0][0] = true;
 
-        if (nums[0] <= target) {
-            dp[0][nums[0]] = true;
-        }
-        for (int i = 1; i < len; i++) {
-            for (int j = 0; j <= target; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (nums[i] <= j) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
-                }
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= target; ++j) {
+                // 不选第 i 件物品
+                boolean no = dp[i - 1][j];
+                // 选第 i 件物品
+                boolean yes = j >= nums[i - 1] ? dp[i - 1][j - nums[i - 1]] : false;
+                // 转移方程：dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]]
+                dp[i][j] = no | yes;
             }
+        }
 
-            // 由于状态转移方程的特殊性，提前结束，可以认为是剪枝操作
-            if (dp[i][target]) {
-                return true;
-            }
-        }
-        return dp[len - 1][target];
+        return dp[n][target];
     }
 
-    /**
-     * 方法三：动态规划 + 空间优化
-     */
-    public boolean canPartition3(int[] nums) {
-        int len = nums.length;
+    public boolean canPartition5(int[] nums) {
+        // 「滚动数组」解法
+        int n = nums.length;
+
+        //「等和子集」的和必然是总和的一半
         int sum = 0;
         for (int num : nums) {
             sum += num;
         }
-        if ((sum & 1) == 1) {
+        int target = sum / 2;
+
+        // 对应了总和为奇数的情况，注定不能被分为两个「等和子集」
+        if (target * 2 != sum) {
             return false;
         }
 
-        int target = sum / 2;
-        boolean[] dp = new boolean[target + 1];
-        dp[0] = true;
+        // dp[i][j] 表示从数组的 [0,i) 下标范围内选取若干个正整数（可以是 0 个），
+        // 是否存在一种选取方案使得被选取的正整数的和等于 j
+        // 修改「物品维度」为 2
+        boolean[][] dp = new boolean[2][target + 1];
 
-        if (nums[0] <= target) {
-            dp[nums[0]] = true;
-        }
-        // 使用一维表格，并且「从后向前」填表格
-        for (int i = 1; i < len; ++i) {
-            for (int j = target; nums[i] <= j; --j) {
-                if (dp[target]) {
-                    return true;
-                }
-                dp[j] = dp[j] || dp[j - nums[i]];
+        // 初始化一个「有效值」
+        dp[0][0] = true;
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= target; ++j) {
+                // 不选第 i 件物品
+                boolean no = dp[(i - 1) & 1][j];
+                // 选第 i 件物品
+                boolean yes = j >= nums[i - 1] ? dp[(i - 1) & 1][j - nums[i - 1]] : false;
+                dp[i & 1][j] = no | yes;
             }
         }
+
+        return dp[n & 1][target];
+    }
+
+    public boolean canPartition6(int[] nums) {
+        // 「一维空间优化」解法
+        int n = nums.length;
+
+        //「等和子集」的和必然是总和的一半
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int target = sum / 2;
+
+        // 对应了总和为奇数的情况，注定不能被分为两个「等和子集」
+        if (target * 2 != sum) {
+            return false;
+        }
+
+        // dp[i][j] 表示从数组的 [0,i) 下标范围内选取若干个正整数（可以是 0 个），
+        // 是否存在一种选取方案使得被选取的正整数的和等于 j
+        // 取消「物品维度」，只保留代表「剩余容量」的维度
+        boolean[] dp = new boolean[target + 1];
+
+        // 初始化一个「有效值」
+        dp[0] = true;
+
+        for (int i = 1; i <= n; ++i) {
+            // 将容量遍历方向修改为「从大到小」
+            for (int j = target; j >=  nums[i - 1]; --j) {
+                // 不选第 i 件物品
+                boolean no = dp[j];
+                // 选第 i 件物品
+                boolean yes = dp[j - nums[i - 1]];
+                dp[j] = no | yes;
+            }
+        }
+
         return dp[target];
     }
 
@@ -122,5 +241,8 @@ class Solution {
         System.out.println(new Solution().canPartition1(new int[]{1, 5, 11, 5}));
         System.out.println(new Solution().canPartition2(new int[]{1, 5, 11, 5}));
         System.out.println(new Solution().canPartition3(new int[]{1, 5, 11, 5}));
+        System.out.println(new Solution().canPartition4(new int[]{1, 2, 3, 5}));
+        System.out.println(new Solution().canPartition5(new int[]{1, 2, 3, 5}));
+        System.out.println(new Solution().canPartition6(new int[]{1, 2, 3, 5}));
     }
 }
