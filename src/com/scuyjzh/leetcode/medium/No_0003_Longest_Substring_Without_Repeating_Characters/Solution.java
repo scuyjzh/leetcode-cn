8985b050ab8bf7f0599c8311504775d9dd147b88
scuyjzh <scuyjzh@163.com>
2022-01-14 16:35:59 +0800

    modify solutions to problem - "3. Longest Substring Without Repeating Characters"

--- src/com/scuyjzh/leetcode/medium/No_0003_Longest_Substring_Without_Repeating_Characters/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0003_Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -8,33 +8,25 @@ import java.util.*;
  * 给定一个字符串，请你找出其中不含有重复字符的最长子串的长度。
  */
 class Solution {
-    /**
-     * 方法：滑动窗口
-     *
-     * • 时间复杂度：O(N)，其中 N 是字符串的长度。左指针和右指针分别会遍历整个字符串一次。
-     * • 空间复杂度：O(|Σ|)，其中 Σ 表示字符集（即字符串中可以出现的字符），|Σ| 表示字符集的大小。在
-     *   本题中没有明确说明字符集，因此可以默认为所有 ASCII 码在 [0,128) 内的字符，即 ∣Σ∣=128。
-     *   需要用到哈希集合来存储出现过的字符，而字符最多有 ∣Σ∣ 个，因此空间复杂度为 O(∣Σ∣)。
-     */
     public int lengthOfLongestSubstring(String s) {
         // 哈希集合，记录每个字符是否出现过
         Set<Character> occ = new HashSet<>();
         int n = s.length();
         // 右指针，初始值为 -1，相当于在字符串的左边界的左侧，还没有开始移动
-        int rk = -1, ans = 0;
+        int right = -1, ans = 0;
         // 左指针，不断将左指针向右移动一格，表示开始枚举下一个字符作为起始位置
-        for (int i = 0; i < n; ++i) {
-            if (i != 0) {
+        for (int left = 0; left < n; ++left) {
+            if (left != 0) {
                 // 左指针向右移动一格，移除一个字符
-                occ.remove(s.charAt(i - 1));
+                occ.remove(s.charAt(left - 1));
             }
-            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+            while (right + 1 < n && !occ.contains(s.charAt(right + 1))) {
                 // 不断地移动右指针
-                occ.add(s.charAt(rk + 1));
-                ++rk;
+                occ.add(s.charAt(right + 1));
+                ++right;
             }
-            // 第 i 到 rk 个字符是一个极长的无重复字符子串
-            ans = Math.max(ans, rk - i + 1);
+            // 第 left 到 right 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, right - left + 1);
         }
         return ans;
     }
