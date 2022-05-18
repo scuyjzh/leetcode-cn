b90943be15684fcd162af100233edf6abcd38a3e
scuyjzh <scuyjzh@163.com>
2022-05-18 17:22:55 +0800

    modify solutions to problem - "36. Valid Sudoku"

--- src/com/scuyjzh/leetcode/medium/No_0036_Valid_Sudoku/Solution.java
+++ src/com/scuyjzh/leetcode/medium/No_0036_Valid_Sudoku/Solution.java
@@ -1,7 +1,5 @@
 package com.scuyjzh.leetcode.medium.No_0036_Valid_Sudoku;
 
-import java.util.*;
-
 /**
  * 36. 有效的数独
  *
@@ -17,54 +15,29 @@ import java.util.*;
  *   • 空白格用 '.' 表示。
  */
 class Solution {
-    /**
-     * 方法：一次迭代
-     *
-     * • 时间复杂度：O(1)。数独共有 81 个单元格，只需要对每个单元格遍历一次即可。
-     * • 空间复杂度：O(1)。由于数独的大小固定，因此哈希表的空间也是固定的。
-     */
     public boolean isValidSudoku(char[][] board) {
-        /*
-         * 一个简单的解决方案是遍历该 9 x 9 数独 三 次，以确保：
-         * 1.行中没有重复的数字。
-         * 2.列中没有重复的数字。
-         * 3.3 x 3 子数独内没有重复的数字。
-         * 实际上，所有这一切都可以在一次迭代中完成。
-         *
-         * 讨论两个问题：
-         * 1.如何枚举子数独？
-         *   - 可以使用 box_index = (row / 3) * 3 + columns / 3，其中 / 是整数除法。
-         *
-         * 2.如何确保行 / 列 / 子数独中没有重复项？
-         *   - 可以利用 value -> count 哈希映射来跟踪所有已经遇到的值。
-         */
-        HashMap<Integer, Integer>[] rows = new HashMap[9];
-        HashMap<Integer, Integer>[] columns = new HashMap[9];
-        HashMap<Integer, Integer>[] subboxes = new HashMap[9];
-        for (int i = 0; i < 9; ++i) {
-            rows[i] = new HashMap<>(9);
-            columns[i] = new HashMap<>(9);
-            subboxes[i] = new HashMap<>(9);
-        }
-        // 遍历数独
+        // 创建二维数组 rows、columns 和 subboxes 分别记录数独的每一行、每一列和每一个小九宫格中的每个数字的出现次数
+        int[][] rows = new int[9][10];
+        int[][] columns = new int[9][10];
+        int[][] subboxes = new int[9][10];
+        // 遍历数独一次，在遍历的过程中更新二维数组中的计数
         for (int i = 0; i < 9; ++i) {
             for (int j = 0; j < 9; ++j) {
                 char ch = board[i][j];
                 if (ch != '.') {
-                    int n = ch - '0';
-                    // idx = ⌊i/3⌋ ∗ 3 + ⌊j/3⌋
-                    int idx = (i / 3) * 3 + j / 3;
-                    // 保留当前单元格值
-                    rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
-                    columns[j].put(n, columns[j].getOrDefault(n, 0) + 1);
-                    subboxes[idx].put(n, subboxes[idx].getOrDefault(n, 0) + 1);
-                    // 检查看到每个单元格值是否已经在当前的行 / 列 / 子数独中出现过
-                    if (rows[i].get(n) > 1 || columns[j].get(n) > 1 || subboxes[idx].get(n) > 1) {
+                    int curNum = ch - '0';
+                    rows[i][curNum]++;
+                    columns[j][curNum]++;
+                    subboxes[(i / 3) * 3 + j / 3][curNum]++;
+                    // 判断是否满足有效的数独的条件
+                    if (rows[i][curNum] > 1 || columns[j][curNum] > 1 || subboxes[(i / 3) * 3 + j / 3][curNum] > 1) {
                         return false;
                     }
                 }
+
             }
         }
+        // 如果遍历结束之后没有出现计数大于 1 的情况，则符合有效的数独的条件，返回 true
         return true;
     }
 
